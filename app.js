0a2052a80aa39805fbd24362d96c713e7300917b
Adri Skrtic <adri.curtis@gmail.com>
2018-01-11 21:28:26 -0600

    using Jade as a templating engine

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 5000;
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine', 'jade');
 app.get('/', function(req, res) {
-    res.send('hi');
+    res.render('index', {list: ['a','b']});
 });
 
 app.get('/books', function(req, res) {
