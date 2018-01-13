77c6bb92be3426bd9d321575688c2a1be6dc54dd
Adri Skrtic <adri.curtis@gmail.com>
2018-01-13 17:07:53 -0600

    buiding out ejs templaing engine support

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var app = express();
 var port = process.env.port || 5000;
 app.use(express.static('public'));
 app.set('views','./src/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-    res.render('index', {list: ['a','b']});
+    res.render('index', {title:'mytitle',list: ['topic a','topic b']});
+});
+app.get('/2', function(req, res) {
+    res.render('index2', {title:'second index',list: ['topic 1','topic b']});
 });
 
 app.get('/books', function(req, res) {
