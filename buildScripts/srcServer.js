07f8b895c03d391ba99e26f2bd430200a89b07d6
Adri Skrtic <adri.curtis@gmail.com>
2018-01-24 20:11:25 -0600

    added .editorconfig, changed app  js file structure; adding build scripts added babel support added some webpack support - only handles some js now, not css

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import bodyParser  from 'body-parser';
+//var cookieParser = require('cookie-parser');
+import passport from 'passport';
+import session from 'express-session';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+const port = process.argv[2] || 5000;
+const app = express();
+
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo:true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(express.static('public'));
+app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+//app.use(cookieParser());
+app.use(session({secret:'mysecretissecret'}));
+require ('../src/config/passport')(app);
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+var nav = [
+  {link: '/authors', text: 'Author'},
+  {link: '/books', text: 'Book'}
+];
+
+var authorRouter = require('../src/routes/authorRoutes')(nav);
+var bookRouter = require('../src/routes/bookRoutes')(nav);
+var adminRouter = require('../src/routes/adminRoutes')(nav);
+var authRouter = require('../src/routes/authRoutes')(nav);
+
+app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
+app.use('/Authors', authorRouter);
+app.use('/Auth', authRouter);
+
+app.get('/', function(req,res) {
+  res.render('index', {
+    title: 'mytitle',
+    nav: [
+      {link: '/authors', text: 'Authors'},
+      {link: '/books', text: 'Books'}
+  ]});
+});
+app.listen(port, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var nodemon = require('gulp-nodemon');
 
-var jsFiles = ['*.js', 'src/**/*.js'];
+var jsFiles = ['*.js', 'src/**/*.js', 'buildScripts/*.js'];
 
 gulp.task('style', function () {
     return gulp.src(jsFiles)
@@ -38,72 +38,36 @@ gulp.task('inject', function () {
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 
-})
+});
 
 //gulp.task('serve', ['style', 'inject'], function () {
-    gulp.task('serve',  function () {
-        var options = {
-        script: 'app.js',
-        delayTime: 1,
-        env: {
-            'PORT': 3000
-        },
-        watch: jsFiles
-    }
-
-    return nodemon(options)
-        .on('restart', function (ev) {
-            console.log('Restarting....');
-        })
-})
-
-/*var gulp = require('gulp');
-var jshint = require ('gulp-jshint');
-var jscs = require('gulp-jscs');
-var nodemon = require('gulp-nodemon');
-
-var jsFiles = ['*.js', 'src/** /*.js'];
-
-gulp.task('style', function() {
-    return gulp.src(jsFiles)
-//        .pipe(jshint())
-  //      .pipe(jshint.reporter('jshint-stylish', {
-    //        verbose:true
-      //  }))
-        .pipe(jscs());
-});
-
-gulp.task('inject', function() {
-    var wireDep = require ('wiredep').stream;
-    var inject = require('gulp-inject');
-    var injectSource = gulp.src(['./public/css/*.css', './public/js/*.js'],
-                                {read:false});
-    var injectOptions = {
-        ignorePath: '/public'
-    };
+  gulp.task('serve',  function () {
     var options = {
-        bowerJson : require('./bower.json'),
-        directory: './public/lib',
-        ignorePath: '../../public'
-    };
-    return gulp.src('./src/views/*.html')
-        .pipe(wireDep(options))
-        .pipe(inject(injectSource, injectOptions))
-        .pipe(gulp.dest('./src/views'));
+      script: 'app.js',
+      delayTime: 1,
+    env: {
+        'PORT': 3000
+    },
+    watch: jsFiles
+};
+
+return nodemon(options)
+    .on('restart', function (ev) {
+        console.log('Restarting....');
+    });
 });
+gulp.task('srcServer',  function () {
+  var options = {
+    script: 'buildscripts/srcServer.js',
+    delayTime: 1,
+  env: {
+      'PORT': 3000
+  },
+  watch: jsFiles
+};
 
-gulp.task('serve', ['style', 'inject'], function () {
-    var options = {
-        script: 'app.js',
-        delayTime: 1,
-        env: {
-            'PORT' : 5000
-        },
-        watch: jsFiles
-    }
-
-    return nodemon(options)
-        .on('restart', function (ev) {
-            console.log('restarting.....');
-        })
-})*/
+return nodemon(options)
+  .on('restart', function (ev) {
+      console.log('Restarting....');
+  });
+});
